e6bc91e72f1463bd3aa90027cd8e19eec8a33214
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-12-11 16:40:50 +0100

    fix perl oddities

--- megaraid-collectd.pl
+++ megaraid-collectd.pl
@@ -2,21 +2,34 @@
 
 use strict;
 use warnings;
+use Env qw($COLLECTD_HOSTNAME $COLLECTD_INTERVAL);
 
-my $hostname = $ENV{'COLLECTD_HOSTNAME'} || `hostname`;
-my $interval = $ENV{'COLLECTD_INTERVAL'} || 900;
+my $hostname;
+my $interval;
 
-$interval = int($interval);
-$hostname = chomp($hostname);
+if (defined($COLLECTD_HOSTNAME)) {
+	printf "yeah!\n";
+	$hostname = $COLLECTD_HOSTNAME;
+} else {
+	chomp($hostname = `hostname`);
+}
+
+if (defined($COLLECTD_INTERVAL)) {
+	$interval = int($COLLECTD_INTERVAL);
+} else {
+	$interval = 900;
+}
+
+printf "hostname=%s\ninterval=%d\n", $hostname, $interval;
 
 while (1) {
 	my $init_stamp = time();
 	my $status = `megaclisas-status --nagios`;
 	if ($status =~ /^RAID ([a-zA-Z]+) - Arrays: OK:(\d+) Bad:(\d+) - Disks: OK:(\d+) Bad:(\d+)/) {
-		printf "PUTVAL \"%s/megaraid/gauge-array_ok\" interval=%d N:%d\n", $hostname, $2, $interval;
-		printf "PUTVAL \"%s/megaraid/gauge-array_bad\" interval=%d N:%d\n", $hostname, $3, $interval;
-		printf "PUTVAL \"%s/megaraid/gauge-disk_ok\" interval=%d N:%d\n", $hostname, $4, $interval;
-		printf "PUTVAL \"%s/megaraid/gauge-disk_bad\" interval=%d N:%d\n", $hostname, $5, $interval;
+		printf "PUTVAL \"%s/megaraid/gauge-array_ok\" interval=%d N:%d\n", $hostname, $interval, $2; 
+		printf "PUTVAL \"%s/megaraid/gauge-array_bad\" interval=%d N:%d\n", $hostname, $interval, $3;
+		printf "PUTVAL \"%s/megaraid/gauge-disk_ok\" interval=%d N:%d\n", $hostname, $interval, $4;
+		printf "PUTVAL \"%s/megaraid/gauge-disk_bad\" interval=%d N:%d\n", $hostname, $interval, $5;
 	}
 	my $end_stamp = time();
 
